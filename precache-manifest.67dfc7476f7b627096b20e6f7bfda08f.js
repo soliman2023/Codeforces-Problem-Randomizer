94f67c75faf37cdc3f987f5716e5b5acda4ef394
unknown <mostafa.a.soliman00@gmail.com>
2024-10-05 20:43:19 +0300

    Updates

---
+++ precache-manifest.67dfc7476f7b627096b20e6f7bfda08f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23918821cb155e56d89ad687da3cf3b1",
+    "url": "/Codeforces-Problem-Randomizer/index.html"
+  },
+  {
+    "revision": "9a9f8c5f9f320971e5cb",
+    "url": "/Codeforces-Problem-Randomizer/static/css/main.84ffe397.chunk.css"
+  },
+  {
+    "revision": "21425b2e4b7bb44e3a45",
+    "url": "/Codeforces-Problem-Randomizer/static/js/2.312a7ca8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Codeforces-Problem-Randomizer/static/js/2.312a7ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a9f8c5f9f320971e5cb",
+    "url": "/Codeforces-Problem-Randomizer/static/js/main.f25e0bc2.chunk.js"
+  },
+  {
+    "revision": "e0143fda21a4c6edc853",
+    "url": "/Codeforces-Problem-Randomizer/static/js/runtime-main.357231e2.js"
+  },
+  {
+    "revision": "a82c4c3bfe6cd8a9d57597071772d0c7",
+    "url": "/Codeforces-Problem-Randomizer/static/media/Kanit.a82c4c3b.ttf"
+  },
+  {
+    "revision": "0113e4ab0457064ceff687ef5f2f52c2",
+    "url": "/Codeforces-Problem-Randomizer/static/media/codeforces_logo.0113e4ab.png"
+  },
+  {
+    "revision": "89c2c0674bdba3d61daf6b1b1e5e82b5",
+    "url": "/Codeforces-Problem-Randomizer/static/media/gh-logo.89c2c067.png"
+  },
+  {
+    "revision": "f2ee10e194f38e0e1dd3575ca760e5db",
+    "url": "/Codeforces-Problem-Randomizer/static/media/loop.f2ee10e1.svg"
+  }
+]);
